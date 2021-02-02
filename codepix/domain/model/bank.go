c358abec75a095433501be43056b2c470696b859
Mateus Barbosa <mateus.barbosa@leucotron.com.br>
2021-02-02 00:37:31 -0300

    Adição de Accounts no modelo Bank

--- codepix/domain/model/bank.go
+++ codepix/domain/model/bank.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Bank struct {
-  Base          `valid:"required"`
-	Code  string  `json: "code" valid:"notnull"`
-  Name  string  `json: "name" valid:"notnull"`
+  Base                  `valid:"required"`
+	Code      string      `json: "code" valid:"notnull"`
+  Name      string      `json: "name" valid:"notnull"`
+  Accounts  []*Account  `valid:"-"`
 }
 
 func (bank *Bank) isValid() error {
