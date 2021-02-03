2f67cb980499020e582144dca5fbd3036b39b80c
Mateus Barbosa <mateus.barbosa@leucotron.com.br>
2021-02-02 23:58:10 -0300

    Inicialização do PixGrpcService no server

--- codepix/application/grpc/server.go
+++ codepix/application/grpc/server.go
@@ -6,11 +6,22 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
+	"github.com/mateusjbarbosa/imersao-fullstack-fullcycle/codepix/application/grpc/pb"
+	"github.com/mateusjbarbosa/imersao-fullstack-fullcycle/codepix/application/usecase"
+	"github.com/mateusjbarbosa/imersao-fullstack-fullcycle/codepix/infrastructure/repository"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
 
 	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
