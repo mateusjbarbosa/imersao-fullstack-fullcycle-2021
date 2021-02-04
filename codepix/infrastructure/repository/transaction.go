af3e6c2ff41266d6351acc7cc5db66c49ad2f3c3
Mateus Barbosa <mateus.barbosa@leucotron.com.br>
2021-02-03 23:56:49 -0300

    Correção do registro do gorm no transactionRepository

--- codepix/infrastructure/repository/transaction.go
+++ codepix/infrastructure/repository/transaction.go
@@ -3,8 +3,8 @@ package repository
 import (
 	"fmt"
 
+	"github.com/jinzhu/gorm"
 	"github.com/mateusjbarbosa/imersao-fullstack-fullcycle/codepix/domain/model"
-	"gorm.io/gorm"
 )
 
 type TransactionRepositoryDb struct {
