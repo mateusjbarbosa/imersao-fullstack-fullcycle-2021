ca100fc4d14d5174b1a2f287908660b3bb2c7431
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-02-07 12:17:17 -0300

    Adição das configurações do TypeOrm no app.module

--- bank-api/src/app.module.ts
+++ bank-api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccount } from './models/bank-account.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [BankAccount],
+    }),
+    TypeOrmModule.forFeature([BankAccount]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
