8a00524b4f5598821bf1a26eea9d16ecb7a7223d
Mateus Barbosa <mateus.barbosa@leucotron.com.br>
2021-02-08 10:54:10 -0300

    Adição do ModelNotFoundException

--- bank-api/src/main.ts
+++ bank-api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ModelNotFoundException } from './exceptions/model-not.found.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  app.useGlobalFilters(new ModelNotFoundException());
 
   await app.listen(3000);
 }
